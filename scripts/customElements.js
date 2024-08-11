09465dbd888c77b6c07cfbdf6ab74ccb23b699cc
notosarme <djinnstreasure@pm.me>
2024-08-11 18:34:50 -0300

    Add styling to card and gallery

--- scripts/customElements.js
+++ scripts/customElements.js
@@ -1,23 +1,30 @@
 class HighlightCard extends HTMLElement {
   constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
-      this.shadowRoot.innerHTML = `
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="/styles/custom.css">
-          <div class="card">
-              <img src="" alt="" />
-              <div class="card-container">
-                <p></p>
-              </div>
-          </div>
+        <div class="card">
+          <a href="">
+            <img src="" alt="" />
+            <div class="card-container">
+              <h3></h3>
+              <p></p>
+            </div>
+          </a>
+        </div>
       `;
   }
 
   connectedCallback() {
-      this.shadowRoot.querySelector('img').src = this.getAttribute('image-src');
-      this.shadowRoot.querySelector('img').alt = this.getAttribute('alt');
-      this.shadowRoot.querySelector('.card-container p').textContent = this.getAttribute('text');
+    this.shadowRoot.querySelector("a").href = this.getAttribute("link");
+    this.shadowRoot.querySelector("img").src = this.getAttribute("image-src");
+    this.shadowRoot.querySelector("img").alt = this.getAttribute("alt");
+    this.shadowRoot.querySelector(".card-container h3").textContent =
+      this.getAttribute("heading");
+    this.shadowRoot.querySelector(".card-container p").textContent =
+      this.getAttribute("text");
   }
 }
 
-customElements.define('highlight-card', HighlightCard);
+customElements.define("highlight-card", HighlightCard);
